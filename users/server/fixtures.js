f28c914eb4af1bb4d44fa27c8cb918164354dc18
schisma <jona@schisma.co>
2015-08-01 15:31:08 +0200

    created some fixtures

---
+++ users/server/fixtures.js
@@ -0,0 +1,8 @@
+if (Users.find().count() === 0) {
+  // if no users exist, create an initial one
+  Accounts.createUser({
+    username: 'admin',
+    email: 'jona@schisma.co',
+    password: 'supersecret'
+  })
+}
