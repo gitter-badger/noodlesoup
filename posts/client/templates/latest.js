28ee649c44205aea4670c88d568b0339e8f698fe
schisma <jona@schisma.co>
2015-08-06 21:09:50 +0200

    misc fixes

--- posts/client/templates/latest.js
+++ posts/client/templates/latest.js
@@ -1,6 +1,7 @@
 Template.postLatest.helpers({
   posts: function () {
-    return Posts.find({}, {limit: 5})
+    // TODO: fetch them by actual date
+    return Posts.find({}, {limit: 5}).fetch().reverse()
   }
 })
 
@@ -8,6 +9,10 @@ Template.postLatestOne.helpers({
   // trim the body size to 200 characters and if it goes over,
   // append a 'read more' link
   trimBody: function (body) {
-    return body.slice(0, 20)
+    var newBody = body.slice(0, 100)
+    if (body.length > 100) {
+      newBody.concat('...')
+    }
+    return newBody
   }
 })
--- posts/client/templates/one.js
+++ posts/client/templates/one.js
@@ -2,5 +2,11 @@ Template.postOne.helpers({
   getGravatarUrl: function (username) {
     var user = Users.findOne({username: username})
     return Gravatar.imageUrl(user.emails[0].address)
+  },
+  isAuthor: function (author) {
+    if (author === Meteor.user().username) {
+      return true
+    }
+    return false
   }
 })
