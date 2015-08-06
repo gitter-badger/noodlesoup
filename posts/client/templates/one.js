9f1c7f7f7f28b2e29ec3d821edb03bdca1cc45d6
schisma <jona@schisma.co>
2015-08-06 18:01:36 +0200

    removed dumb debug line

--- posts/client/templates/one.js
+++ posts/client/templates/one.js
@@ -1,7 +1,6 @@
 Template.postOne.helpers({
   getGravatarUrl: function (username) {
     var user = Users.findOne({username: username})
-    console.log(user.emails)
     return Gravatar.imageUrl(user.emails[0].address)
   }
 })
