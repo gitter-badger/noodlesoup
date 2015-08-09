4b805f2fd7f142a11ccf13c836ae480a704cf00d
schisma <jona@schisma.co>
2015-08-10 01:55:33 +0200

    stricter security rules

--- core/server/security.js
+++ core/server/security.js
@@ -1,2 +1,11 @@
+Security.defineMethod('ifIsCurrentUser', {
+  fetch: [],
+  transform: null,
+  deny: function (type, arg, userId, doc) {
+    return userId !== doc._id
+  }
+})
+
 Posts.permit(['insert', 'update', 'remove']).ifLoggedIn().apply()
-Users.permit(['insert', 'update', 'remove']).ifLoggedIn().apply()
+Users.permit(['insert']).ifLoggedIn().apply()
+Users.permit(['update', 'remove']).ifIsCurrentUser().ifLoggedIn().apply()
