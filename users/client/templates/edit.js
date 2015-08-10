7d7be4cc7e9847b6662c0610609203f1abc0fa23
schisma <jona@schisma.co>
2015-08-10 02:01:07 +0200

    added password change

--- users/client/templates/edit.js
+++ users/client/templates/edit.js
@@ -8,13 +8,19 @@ Template.userEdit.helpers({
 })
 
 Template.userEdit.events({
-  'submit form': function (event) {
+  'submit #updateUserForm': function (event) {
     event.preventDefault()
     // set to the current value
     var realname = event.target.realname.value || Users.find({_id: Meteor.userId()}).fetch()[0].profile.realname
       , bio = event.target.bio.value || Users.find({_id: Meteor.userId()}).fetch()[0].profile.bio
     
     Users.update({_id: Meteor.userId()}, {$set: {profile: {realname: realname, bio: bio}}})
+    Router.go('/')
+  },
+  'submit #updateUserPasswordForm': function (event) {
+    event.preventDefault()
+
+    Accounts.changePassword(event.target.oldPassword.value, event.target.newPassword.value)
     Router.go('/')
   }
 })
