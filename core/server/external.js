d4c9e65c4844ab0bff344dca3a41b97b8f75cd98
schisma <jona@schisma.co>
2015-08-15 12:39:10 +0200

    conditional kadira connection establishment

--- core/server/external.js
+++ core/server/external.js
@@ -1,1 +1,3 @@
-//Kadira.connect('JCWz6EfT5s79cmw8B', '83f2addd-c55c-4c34-828f-ac5314390bfc')
+if (!process.env.ROOT_URL === 'http://localhost:3000') {
+  Kadira.connect('JCWz6EfT5s79cmw8B', '83f2addd-c55c-4c34-828f-ac5314390bfc')
+}
