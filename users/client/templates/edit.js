9ae7f41ae1b326ee9698b09c17b1e7deb3c31e67
schisma <jona@schisma.co>
2015-08-23 14:03:01 +0200

    added username and email update

--- users/client/templates/edit.js
+++ users/client/templates/edit.js
@@ -1,9 +1,15 @@
 Template.userEdit.helpers({
   getRealname: function () {
-    return Users.find({_id: Meteor.userId()}).fetch()[0].profile.realname
+    return Users.findOne({_id: Meteor.userId()}).profile.realname
   },
   getBio: function () {
-    return Users.find({_id: Meteor.userId()}).fetch()[0].profile.bio
+    return Users.findOne({_id: Meteor.userId()}).profile.bio
+  },
+  getEmail: function () {
+    return Users.findOne({_id: Meteor.userId()}).emails[0].address
+  },
+  getUsername: function () {
+    return Users.findOne({_id: Meteor.userId()}).username
   }
 })
 
@@ -11,10 +17,12 @@ Template.userEdit.events({
   'submit #updateUserForm': function (event) {
     event.preventDefault()
     // set to the current value
-    var realname = event.target.realname.value || Users.find({_id: Meteor.userId()}).fetch()[0].profile.realname
-      , bio = event.target.bio.value || Users.find({_id: Meteor.userId()}).fetch()[0].profile.bio
+    var realname = event.target.realname.value || Users.findOne({_id: Meteor.userId()}).profile.realname
+      , bio = event.target.bio.value || Users.findOne({_id: Meteor.userId()}).profile.bio
+      , username = event.target.username.value || Users.findOne({_id: Meteor.userId()}).username
+      , email = event.target.email.value || Users.findOne({_id: Meteor.userId()}).emails[0].address
     
-    Users.update({_id: Meteor.userId()}, {$set: {profile: {realname: realname, bio: bio}}})
+    Users.update({_id: Meteor.userId()}, {$set: {username: username, 'emails.0.address': email, profile: {realname: realname, bio: bio}}})
     Router.go('/')
   },
   'submit #updateUserPasswordForm': function (event) {
