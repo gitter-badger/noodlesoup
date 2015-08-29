113da0087d1034351edb715c4532cd7a4a2decfb
schisma <jona@schisma.co>
2015-08-30 01:19:04 +0200

    (hopefully) fixed mongodb inconsistencies

--- server/hooks.js
+++ server/hooks.js
@@ -4,6 +4,12 @@
 
 
 Accounts.onCreateUser(function (opts, user) {
+  // hooray for mongodb stability!
+  if (!user.profile) {
+    user.profile = {}
+    user.profile.realname = ""
+    user.profile.bio = ""
+  }
   Tokens.remove({})
   return user
 })
