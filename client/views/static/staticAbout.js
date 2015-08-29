212a507d709aa542c83baa788fa62e8cb99bf899
schisma <jona@schisma.co>
2015-08-30 00:35:59 +0200

    randomize users on about
    nobody shall be prefered, this is the age of equality

--- client/views/static/staticAbout.js
+++ client/views/static/staticAbout.js
@@ -1,6 +1,20 @@
+// Lightweight version of the Fisher-Yates shuffle
+function fyShuffle (array) {  
+  var currentIndex = array.length, temporaryValue, randomIndex
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+  return array
+} 
+
 Template.staticAbout.helpers({
   users: function () {
-    return Users.find()
+    var users = Users.find().fetch()
+    return fyShuffle(users)
   },
   getGravatarUrl: function (emails) {
     return Gravatar.imageUrl(emails[0].address, {
